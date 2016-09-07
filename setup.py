40062cf8ddba981cc921356713a704673581d00d
Nathan Buckner <nathan.buckner@rackspace.com>
2016-09-07 15:34:18 -0500

    updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
 setup(
     name="logtail",
-    version="0.0.3",
+    version="0.0.4",
     description="Package for tailing logs and viewing in editor",
     long_description=open("README.md").read(),
     author="Nathan Buckner",
